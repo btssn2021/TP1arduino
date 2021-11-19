20d8bd2d3f57308f3a277087eee4fc7a10951459
eric <eric.thomas@formation-industries-fc.fr>
2021-11-19 16:06:44 +0100

    Montage n°2

--- test1.ino
+++ test1.ino
@@ -1,15 +1,23 @@
 #include <Arduino.h>
-const int PINDIGITAL=12;
+enum {LR1=12,LR2=11,LR3=10,LR4=9};
 void setup()
 {
-    pinMode(PINDIGITAL,OUTPUT); //met la pin 12 en mode OUTPUT
-
+    pinMode(LR1,OUTPUT); //met la pin 12 en mode OUTPUT
+    pinMode(LR2,OUTPUT);
+    pinMode(LR3,OUTPUT);
+    pinMode(LR4,OUTPUT);
 }
 
 void loop() {
-    digitalWrite(PINDIGITAL,HIGH); //Allume la led
+    digitalWrite(LR1,HIGH); //Allume la led
+    digitalWrite(LR2,HIGH); //Allume la led
+    digitalWrite(LR3,HIGH); //Allume la led
+    digitalWrite(LR4,HIGH); //Allume la led
     delay(1000);
-    digitalWrite(PINDIGITAL,LOW);  //eteind la led
+    digitalWrite(LR1,LOW);  //eteint la led
+    digitalWrite(LR2,LOW);
+    digitalWrite(LR3,LOW);
+    digitalWrite(LR4,LOW);
     delay(1000);
 
 }
